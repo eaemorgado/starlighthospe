8a55ff9991cb9e0aca833874f715ff7dd5286ccb
Gabriel Morgado <eaemorgado@gmail.com>
2023-10-26 22:59:38 -0300

    s

--- app/routes/router.js
+++ app/routes/router.js
@@ -48,13 +48,6 @@ CREATE TABLE noticia (
 `;
 
 
-connection.query(createTableQuery, (err, result) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log("Tabela criada com sucesso!");
-  }
-});
 
 
 
